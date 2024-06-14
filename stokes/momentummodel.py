b80ac792f5109ec8b5fe6c9b768f6fc621c102eb
amyjenson <ajjenson@alaska.edu>
2024-06-14 13:48:03 -0800

    added eq 3 to solve for d, div issues

--- stokes/momentummodel.py
+++ stokes/momentummodel.py
@@ -12,47 +12,52 @@ import firedrake as fd
 from firedrake.petsc import PETSc, OptionsManager
 
 # public data
-secpera = 31556926.0    # seconds per year
-dayspera = 365.2422     # days per year
+secpera = 31556926.0  # seconds per year
+dayspera = 365.2422  # days per year
 
 # solver packages
 # notes on FGMRES:  1. needed if fieldsplit_0,1_ksp_type is *not* preonly
 #                   2. always applies PC on right (-s_ksp_pc_side right)
-_SchurDirect = {"ksp_type": "fgmres",
-          "pc_type": "fieldsplit",
-          "pc_fieldsplit_type": "schur",
-          "pc_fieldsplit_schur_factorization_type": "full",  # or "diag"
-          "pc_fieldsplit_schur_precondition": "a11",  # the default
-          "fieldsplit_0_ksp_type": "preonly",
-          "fieldsplit_0_pc_type": "lu",
-          "fieldsplit_0_pc_factor_mat_solver_type": "mumps",
-          "fieldsplit_1_ksp_rtol": 1.0e-3,
-          "fieldsplit_1_ksp_type": "gmres",
-          "fieldsplit_1_pc_type": "none"}
-_Direct = {"mat_type": "aij",
-          "ksp_type": "preonly",
-          "pc_type": "lu",
-          "pc_factor_shift_type": "inblocks",
-          "pc_factor_mat_solver_type": "mumps"}
+_SchurDirect = {
+    "ksp_type": "fgmres",
+    "pc_type": "fieldsplit",
+    "pc_fieldsplit_type": "schur",
+    "pc_fieldsplit_schur_factorization_type": "full",  # or "diag"
+    "pc_fieldsplit_schur_precondition": "a11",  # the default
+    "fieldsplit_0_ksp_type": "preonly",
+    "fieldsplit_0_pc_type": "lu",
+    "fieldsplit_0_pc_factor_mat_solver_type": "mumps",
+    "fieldsplit_1_ksp_rtol": 1.0e-3,
+    "fieldsplit_1_ksp_type": "gmres",
+    "fieldsplit_1_pc_type": "none",
+}
+_Direct = {
+    "mat_type": "aij",
+    "ksp_type": "preonly",
+    "pc_type": "lu",
+    "pc_factor_shift_type": "inblocks",
+    "pc_factor_mat_solver_type": "mumps",
+}
 
-packagedict = {"SchurDirect": _SchurDirect,
-               "Direct":      _Direct}
+packagedict = {"SchurDirect": _SchurDirect, "Direct": _Direct}
 
-def D(U):    # strain-rate tensor from velocity U
+
+def D(U):  # strain-rate tensor from velocity U
     return 0.5 * (fd.grad(U) + fd.grad(U).T)
 
+
 # Define a max function for later
-def Max(a, b): 
-    return (a+b+abs(a-b))/fd.Constant(2)    
+def Max(a, b):
+    return (a + b + abs(a - b)) / fd.Constant(2)
+
 
 class MomentumModel(OptionsManager):
-
     def __init__(self, **kwargs):
         # physical constants (private)
-        self._g = 9.81                # m s-2
-        self._rho = 910.0             # kg m-3
-        self._A3 = 3.1689e-24         # Pa-3 s-1; EISMINT I value of ice softness
-        self._B3 = self._A3**(-1.0/3.0)    # Pa s(1/3);  ice hardness
+        self._g = 9.81  # m s-2
+        self._rho = 910.0  # kg m-3
+        self._A3 = 3.1689e-24  # Pa-3 s-1; EISMINT I value of ice softness
+        self._B3 = self._A3 ** (-1.0 / 3.0)  # Pa s(1/3);  ice hardness
         self.n_glen = 3.0
         self.eps = kwargs.pop("eps", 0.01)
         self.alpha = kwargs.pop("alpha", 0.1)
@@ -68,29 +73,45 @@ class MomentumModel(OptionsManager):
     # compute slab-on-slope inflow velocity; note alpha = 0 ==> uin = 0
     def _get_uin(self, mesh):
         _, z = fd.SpatialCoordinate(mesh)
-        C = (2.0 / (self.n_glen + 1.0)) \
-            * (self._rho * self._g * np.sin(self.alpha) / self._B3)**self.n_glen
-        uin = fd.as_vector([C * (self.Hin**(self.n_glen+1.0) \
-                                - (self.Hin - z)**(self.n_glen+1.0)),
-                            0.0])
+        C = (2.0 / (self.n_glen + 1.0)) * (
+            self._rho * self._g * np.sin(self.alpha) / self._B3
+        ) ** self.n_glen
+        uin = fd.as_vector(
+            [
+                C
+                * (
+                    self.Hin ** (self.n_glen + 1.0)
+                    - (self.Hin - z) ** (self.n_glen + 1.0)
+                ),
+                0.0,
+            ]
+        )
         return uin
 
     def create_mixed_space(self, mesh):
-        self._V = fd.VectorFunctionSpace(mesh, 'CG', 2)
-        self._W = fd.FunctionSpace(mesh, 'CG', 1)
-        self._Z = self._V * self._W
+        self._V = fd.VectorFunctionSpace(mesh, "CG", 2)
+        self._W = fd.FunctionSpace(mesh, "CG", 1)
+        self._W2 = fd.FunctionSpace(mesh, "CG", 1)
+        self._Z = self._V * self._W * self._W2
 
-    def solve(self, mesh, bdryids, extrudemode = False, package = 'SchurDirect',
-              upold = None, upcoarse = None):
+    def solve(
+        self,
+        mesh,
+        bdryids,
+        extrudemode=False,
+        package="SchurDirect",
+        upold=None,
+        upcoarse=None,
+    ):
         # define body force and ice hardness
         rhog = self._rho * self._g
-        f_body = fd.Constant((rhog * np.sin(self.alpha), - rhog * np.cos(self.alpha)))
+        f_body = fd.Constant((rhog * np.sin(self.alpha), -rhog * np.cos(self.alpha)))
 
         if self.Hout >= 1.0:
             # if there is an outflow boundary then it is nonhomogeneous Neumann
             # and part of the weak form; we apply hydrostatic normal force
             _, z = fd.SpatialCoordinate(mesh)
-            #outflow_sigma = fd.as_vector( \
+            # outflow_sigma = fd.as_vector( \
             #    [- rhog * np.cos(self.alpha) * (self.Hout - z),
             #    rhog * np.sin(self.alpha) * (self.Hout - z)])
             # Replace outflow boundary stresses with zeros.
@@ -103,38 +124,74 @@ class MomentumModel(OptionsManager):
             self.create_mixed_space(mesh)
             up = fd.Function(self._Z)
             if upcoarse:
-                fd.prolong(upcoarse,up)
-        u, p = fd.split(up)  # get component ufl expressions to define form
-        self.u, self.p = up.subfunctions
-        self.u.rename('velocity')
-        self.p.rename('pressure')
+                fd.prolong(upcoarse, up)
+        u, p, d = fd.split(up)  # get component ufl expressions to define form
+        self.u, self.p, self.d = up.subfunctions
+        self.u.rename("velocity")
+        self.p.rename("pressure")
+        self.d.rename("damage")
 
         # define the nonlinear weak form F(u,p;v,q)
-        v, q = fd.TestFunctions(self._Z)
-        Du2 = 0.5 * fd.inner(D(u), D(u)) + (self.eps * self.Dtyp)**2.0
-        rr = 1.0/self.n_glen - 1.0
-        F = ( fd.inner(self._B3 * Du2**(rr/2.0) * D(u), D(v)) \
-              - p * fd.div(v) - fd.div(u) * q - fd.inner(f_body, v) ) * fd.dx(degree=4)
+        v, q, s = fd.TestFunctions(self._Z)
+        Du2 = (
+            0.5 * fd.inner((1 - d) * D(u), (1 - d) * D(u))
+            + (self.eps * self.Dtyp) ** 2.0
+        )
+        rr = 1.0 / self.n_glen - 1.0
+        nu = 0.5 * self._B3 * Du2 ** (rr / 2.0)
+        dev_sigma = 2 * nu * Du2
+        dev_sigma2 = 0.5 * fd.inner(dev_sigma, dev_sigma)
+        stress = -np.identity(2) * p + dev_sigma
+
+        s11 = stress[0, 0]
+        s13 = stress[1, 0]
+        s33 = stress[1, 1]
+
+        eig1 = 0.5 * (
+            s11 + s33 + ((s11 + s33) ** 2 - 4 * (s11 * s33 - s13**2)) ** 0.5
+        )
+        eig2 = 0.5 * (
+            s11 + s33 - ((s11 + s33) ** 2 - 4 * (s11 * s33 - s13**2)) ** 0.5
+        )
+
+        max_eig = Max(eig1, eig2)
+
+        chi = (
+            self.alpha_coeff * max_eig
+            + (1 - self.alpha_coeff) * (3 * dev_sigma2) ** 0.5
+        )
+
+        k = self.a1 + self.a2 * (3 * dev_sigma2) ** 0.5
+        f = self.B * (chi**self.gamma) / (1 - d) ** k
+        F = (
+            fd.inner(self._B3 * Du2 ** (rr / 2.0) * (1 - d) * D(u), D(v))
+            - p * fd.div(v)
+            - fd.div(u) * q
+            - d * fd.div(u) * s
+            + fd.inner(f, s)
+            - fd.inner(f_body, v)
+        ) * fd.dx(degree=5)
         if self.Hout >= 1.0:
             if extrudemode:
                 # see bottom page https://www.firedrakeproject.org/extruded-meshes.html
-                F -= fd.inner(outflow_sigma, v) * fd.ds_v(bdryids['outflow'])
+                F -= fd.inner(outflow_sigma, v) * fd.ds_v(bdryids["outflow"])
             else:
-                F -= fd.inner(outflow_sigma, v) * fd.ds(bdryids['outflow'])
+                F -= fd.inner(outflow_sigma, v) * fd.ds(bdryids["outflow"])
 
         # Dirichlet boundary conditions
         noslip = fd.Constant((0.0, 0.0))
         inflow_u = self._get_uin(mesh)
-        bcs = [ fd.DirichletBC(self._Z.sub(0), noslip, bdryids['base']),
-                fd.DirichletBC(self._Z.sub(0), inflow_u, bdryids['inflow']) ]
+        bcs = [
+            fd.DirichletBC(self._Z.sub(0), noslip, bdryids["base"]),
+            fd.DirichletBC(self._Z.sub(0), inflow_u, bdryids["inflow"]),
+        ]
 
         # solve
         params = packagedict[package]
         # note: the Firedrake default is 'basic', i.e. no linesearch
         #       we need 'bt' linesearch for n = 3
-        params['snes_linesearch_type'] = 'bt'
-        fd.solve(F == 0, up, bcs=bcs, options_prefix='s',
-                 solver_parameters=params)
+        params["snes_linesearch_type"] = "bt"
+        fd.solve(F == 0, up, bcs=bcs, options_prefix="s", solver_parameters=params)
 
         return up
 
@@ -144,39 +201,41 @@ class MomentumModel(OptionsManager):
     #   pav     = average pressure
     #   pmax    = maximum pressure
     def solutionstats(self, mesh):
-        P1 = fd.FunctionSpace(mesh, 'CG', 1)
-        R = fd.FunctionSpace(mesh, 'R', 0)
+        P1 = fd.FunctionSpace(mesh, "CG", 1)
+        R = fd.FunctionSpace(mesh, "R", 0)
         one = fd.Function(R).assign(1.0)
         area = fd.assemble(one * fd.dx)
         pav = fd.assemble(fd.sqrt(fd.dot(self.p, self.p)) * fd.dx) / area
         with self.p.dat.vec_ro as vp:
             pmax = vp.max()[1]
         umagav = fd.assemble(fd.sqrt(fd.dot(self.u, self.u)) * fd.dx) / area
-        umag = fd.Function(P1).interpolate(fd.sqrt(fd.dot(self.u,self.u)))
+        umag = fd.Function(P1).interpolate(fd.sqrt(fd.dot(self.u, self.u)))
         with umag.dat.vec_ro as vumag:
             umagmax = vumag.max()[1]
-        return umagav,umagmax,pav,pmax
+        return umagav, umagmax, pav, pmax
 
     # generate regularized effective viscosity from the solution:
     #   nu = (1/2) B_n X^((1/n)-1)
     # where X = sqrt(|Du|^2 + eps^2 Dtyp^2)
     def effectiveviscosity(self, mesh):
-        P1 = fd.FunctionSpace(mesh, 'CG', 1)
-        Du2 = 0.5 * fd.inner(D(self.u), D(self.u)) + (self.eps * self.Dtyp)**2.0
+        P1 = fd.FunctionSpace(mesh, "CG", 1)
+        Du2 = (
+            0.5 * fd.inner((1 - self.d) * D(self.u), (1 - self.d) * D(self.u))
+            + (self.eps * self.Dtyp) ** 2.0
+        )
         rr = 1.0 / self.n_glen - 1.0
-        nu = fd.Function(P1).interpolate(0.5 * self._B3 * Du2**(rr/2.0))
-        nu.rename('effective viscosity')
+        nu = fd.Function(P1).interpolate(0.5 * self._B3 * Du2 ** (rr / 2.0))
+        nu.rename("effective viscosity")
         return nu
 
     # generate damage field:
     def damage(self, mesh):
-        P1 = fd.FunctionSpace(mesh, 'CG', 1)
+        P1 = fd.FunctionSpace(mesh, "CG", 1)
         Du2 = 0.5 * fd.inner(D(self.u), D(self.u)) + (self.eps * self.Dtyp) ** 2.0
         rr = 1.0 / self.n_glen - 1.0
         nu = 0.5 * self._B3 * Du2 ** (rr / 2.0)
 
         # deviatoric tensor from velocity U
-        # dev_sigma = (1 / self._A3) * (Du2 ** (rr / 2)) * D(self.u)
         dev_sigma = 2 * nu * Du2
 
         # second invariant of deviatoric tensor from velocity U
@@ -184,47 +243,46 @@ class MomentumModel(OptionsManager):
 
         # cauchy stress tensor
         stress = -np.identity(2) * self.p + dev_sigma
-        #stress = -np.identity(2) * self.p + 2 * Du2 * nu
 
         s11 = stress[0, 0]
         s13 = stress[1, 0]
         s33 = stress[1, 1]
 
-        #eig1 = fd.Function(P1).interpolate(0.5 * (s11 + s33 + ((s11 + s33) ** 2 - 4 * (s11 * s33 - s13**2)) ** 0.5))
-        #eig2 = fd.Function(P1).interpolate(0.5 * (s11 + s33 - ((s11 + s33) ** 2 - 4 * (s11 * s33 - s13**2)) ** 0.5))
-        eig1 = 0.5 * (s11 + s33 + ((s11 + s33) ** 2 - 4 * (s11 * s33 - s13**2)) ** 0.5)
-        eig2 = 0.5 * (s11 + s33 - ((s11 + s33) ** 2 - 4 * (s11 * s33 - s13**2)) ** 0.5)
+        eig1 = 0.5 * (
+            s11 + s33 + ((s11 + s33) ** 2 - 4 * (s11 * s33 - s13**2)) ** 0.5
+        )
+        eig2 = 0.5 * (
+            s11 + s33 - ((s11 + s33) ** 2 - 4 * (s11 * s33 - s13**2)) ** 0.5
+        )
 
         max_eig = Max(eig1, eig2)
 
         chi = fd.Function(P1).interpolate(
-            self.alpha_coeff * max_eig + (1 - self.alpha_coeff) * (3 * dev_sigma2)**0.5
+            self.alpha_coeff * max_eig
+            + (1 - self.alpha_coeff) * (3 * dev_sigma2) ** 0.5
         )
 
-        # check if chi is positive
-        # if chi > 0:
-        #     print("chi > 0")
-
         k = self.a1 + self.a2 * (3 * dev_sigma2) ** 0.5
-        d = 1e-15
-        f = fd.Function(P1).interpolate(self.B * (chi**self.gamma) / (1 - d) ** k)
+        f = fd.Function(P1).interpolate(
+            self.B * (chi**self.gamma) / (1 - self.d) ** k
+        )
         f.rename("damage")
         chi.rename("chi")
-        #return f
+        # return f
         return f, chi
 
     # numerical errors relative to slab-on-slope solution:
     #   uerrmax = maximum magnitude of velocity error
     #   perrmax = maximum of pressure error
-    def numerical_errors_slab(self,mesh):
-        P1 = fd.FunctionSpace(mesh, 'CG', 1)
+    def numerical_errors_slab(self, mesh):
+        P1 = fd.FunctionSpace(mesh, "CG", 1)
         up_exact = fd.Function(self._Z)
         u_exact, p_exact = up_exact.subfunctions
         u_exact.interpolate(self._get_uin(mesh))
         _, z = fd.SpatialCoordinate(mesh)
         p_exact.interpolate(self._rho * self._g * np.cos(self.alpha) * (self.Hin - z))
-        uexactnorm = fd.norm(u_exact, norm_type='L2')
-        uerr = fd.errornorm(u_exact, self.u, norm_type='L2')
-        pexactnorm = fd.norm(p_exact, norm_type='L2')
-        perr = fd.errornorm(p_exact, self.p, norm_type='L2')
+        uexactnorm = fd.norm(u_exact, norm_type="L2")
+        uerr = fd.errornorm(u_exact, self.u, norm_type="L2")
+        pexactnorm = fd.norm(p_exact, norm_type="L2")
+        perr = fd.errornorm(p_exact, self.p, norm_type="L2")
         return uexactnorm, uerr, pexactnorm, perr
